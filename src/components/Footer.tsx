27552cde8bf46d74cf722a4a9e0e97b21c9d16bd
Adam Herdman <cleffei@hotmail.com>
2025-02-16 01:11:27 +0000

    Footer mobile view cleanup

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,30 +5,30 @@ const Footer: React.FC = () => {
   return (
     <section id="footer" className="bg-gray-800">
       <div className="border-t border-amber-500">
-        <div className="container flex flex-col bg-gray-800 items-center px-6 pb-5 mx-auto mt-0 md:space-y-0 md:flex-row md:flex-wrap">
-          <div className="flex justify-between mt-5 md:w-full">
+        <div className="container flex flex-col bg-gray-800 items-center px-6 pb-5 mx-auto mt-0 md:flex-row md:flex-wrap">
+          <div className="flex flex-col items-center text-center mt-5 md:w-full md:flex-row md:justify-between">
             <div className="flex flex-col">
-              <p className="max-w-full text-2xl font-bold text-amber-300 italic text-center md:text-center">
+              <p className="max-w-full text-2xl font-bold text-amber-300 italic">
                 Website Designed & Built by Adam Herdman
               </p>
-              <p className="text-center text-1xl italic text-amber-300">2025</p>
+              <p className="text-amber-300 italic mt-1">2025</p>
             </div>
-            <div className="flex space-x-4">
+            <div className="flex space-x-6 mt-4 md:mt-0">
               <a
                 href="https://github.com/cleffei"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-amber-300 hover:text-amber-500"
+                className="text-amber-300 hover:text-amber-500 transition duration-300"
               >
-                <FaGithub size={50} />
+                <FaGithub size={40} />
               </a>
               <a
                 href="https://www.linkedin.com/in/adam-herdman1999/"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-amber-300 hover:text-amber-500"
+                className="text-amber-300 hover:text-amber-500 transition duration-300"
               >
-                <FaLinkedin size={50} />
+                <FaLinkedin size={40} />
               </a>
             </div>
           </div>
