b633e4c8fcb86f128680417f9253335d4f8b969e
Adam Herdman <cleffei@hotmail.com>
2025-02-16 00:35:10 +0000

    Added ScrollRefresh to always load pages at top

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import TopBar from '../components/TopBar';
 import Footer from '../components/Footer';
+import ScrollRefresh from '../components/ScrollRefresh';
 
 const MainLayout: React.FC = () => {
   return (
     <>
+      <ScrollRefresh />
       <TopBar />
       <Outlet />
       <Footer />
