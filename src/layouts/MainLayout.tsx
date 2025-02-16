b737b85c9116c53f6ee25116a9b3efa78e66fe3c
Adam Herdman <cleffei@hotmail.com>
2025-02-16 01:20:34 +0000

    Fix footer to bottom of page

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,12 +6,14 @@ import ScrollRefresh from '../components/ScrollRefresh';
 
 const MainLayout: React.FC = () => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <ScrollRefresh />
       <TopBar />
-      <Outlet />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
