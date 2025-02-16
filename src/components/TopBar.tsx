2db64878803fcc329741846fabf3c2b26edf4f81
Adam Herdman <cleffei@hotmail.com>
2025-02-16 01:09:15 +0000

    Mobile hamburger menu added

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const TopBar: React.FC = () => {
   const [isVisible, setIsVisible] = useState(true);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleScroll = () => {
     const atTop = window.scrollY === 0;
@@ -15,6 +16,10 @@ const TopBar: React.FC = () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
+
+  const toggleHamMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   const linkClass = ({ isActive }: { isActive: boolean }) =>
     isActive
@@ -31,16 +36,19 @@ const TopBar: React.FC = () => {
       <h1 className="text-center py-2 text-amber-300 font-semibold ml-5">
         Quick Navigation
       </h1>
-      <ul className="flex text-center py-2 space-x-4 font-semibold mr-5">
+      <ul className="hidden md:flex text-center py-2 space-x-4 font-semibold mr-5">
         <li>
           <NavLink to="/" className={linkClass}>
             HOME
           </NavLink>
         </li>
         <li>
-          <NavLink to="#projects" className={linkClass}>
+          <a
+            href="#projects"
+            className="text-amber-300 hover:bg-amber-400 hover:text-white rounded-md px-3 py-2"
+          >
             PROJECTS
-          </NavLink>
+          </a>
         </li>
         <li>
           <NavLink to="/about-me" className={linkClass}>
@@ -48,6 +56,37 @@ const TopBar: React.FC = () => {
           </NavLink>
         </li>
       </ul>
+
+      <button
+        onClick={toggleHamMenu}
+        className="block hamburger md:hidden focus:outline-none pt-3 pr-11"
+      >
+        <div className={`hamburger ${menuOpen ? 'open' : ''}`}>
+          <span className="hamburger-top"></span>
+          <span className="hamburger-middle"></span>
+          <span className="hamburger-bottom"></span>
+        </div>
+      </button>
+
+      <div
+        className={`absolute top-14 left-0 right-0 bg-gray-800 drop-shadow-md border border-b-amber-500 border-t-gray-800 flex flex-col items-center py-6 space-y-4 font-bold transition-all ${
+          menuOpen ? 'block' : 'hidden'
+        } md:hidden`}
+      >
+        <NavLink to="/" className="text-amber-300" onClick={toggleHamMenu}>
+          Home
+        </NavLink>
+        <a href="#projects" className="text-amber-300" onClick={toggleHamMenu}>
+          Projects
+        </a>
+        <NavLink
+          to="/about-me"
+          className="text-amber-300"
+          onClick={toggleHamMenu}
+        >
+          About Me
+        </NavLink>
+      </div>
     </div>
   );
 };
